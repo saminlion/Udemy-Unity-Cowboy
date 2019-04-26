794fcf744bec7bdee12328abf015f950fb107b7b
swh5246 <swh5246@gmail.com>
2019-04-26 21:16:26 +0900

    File Upload

---
+++ Assets/Scripts/CowBoy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System;
+
+public class CowBoy : MonoBehaviourPun {
+
+    public float MoveSpeed = 5;
+    public GameObject playerCam;
+    public SpriteRenderer sprite;
+
+	// Use this for initialization
+	void Awake () {
+        if (photonView.IsMine)
+        { 
+         playerCam.SetActive(true);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (photonView.IsMine)
+        {
+            checkInputs();
+        }
+    }
+
+    private void checkInputs()
+    {
+        var movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0);
+        transform.position += movement * MoveSpeed * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            sprite.flipX = false;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            sprite.flipX = true;
+        }
+    }
+}
---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviourPunCallbacks {
+
+    [SerializeField]
+    private GameObject UserNameScreen, ConnectScreen;
+
+    [SerializeField]
+    private GameObject CreateUserNameButton;
+
+    [SerializeField]
+    private InputField UserNameInput, CreateRoomInput, JoinRoomInput;
+
+
+    void Awake()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to masterr!!!!!");
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Connected to Lobby!!!");
+        UserNameScreen.SetActive(true);
+    }
+    public override void OnJoinedRoom()
+    {
+        // play game scene
+        PhotonNetwork.LoadLevel(1);
+    }
+    #region UIMethods
+
+
+    public void OnClick_CreateNameBtn()
+    {
+       
+        PhotonNetwork.NickName = UserNameInput.text;
+        UserNameScreen.SetActive(false);
+        ConnectScreen.SetActive(true);
+    }
+
+    public void OnNameField_Changed()
+    {
+        if (UserNameInput.text.Length >= 2)
+        {
+            CreateUserNameButton.SetActive(true);
+        }
+        else {
+            CreateUserNameButton.SetActive(false);
+        }
+    }
+
+    public void Onclick_JoinRoom()
+    {
+        RoomOptions ro = new RoomOptions();
+        ro.MaxPlayers = 4;
+        PhotonNetwork.JoinOrCreateRoom(JoinRoomInput.text, ro, TypedLobby.Default);
+
+    }
+    public void Onclick_CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(CreateRoomInput.text, new RoomOptions { MaxPlayers = 4 }, null);
+ }
+    #endregion
+}
